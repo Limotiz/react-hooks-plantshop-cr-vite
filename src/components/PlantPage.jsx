b7490617e0b9ed4d1b351d16a903cf10f5bd8742
david musembi <dmusembi702@gmail.com>
2026-02-21 01:20:32 +0300

    updated components

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -4,11 +4,36 @@ import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  //fetching plants 
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  //adding plant to state
+   function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant]);
+  }
+ //toggling sold out
+  function handleUpdatePlant(updatedPlant) {
+    const updatedPlants = plants.map((plant) =>
+      plant.id === updatedPlant.id ? updatedPlant : plant
+    );
+    setPlants(updatedPlants);
+  }
+  //filtering plants by search
+   const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={filteredPlants} onUpdatedPlant={handleUpdatePlant}/>
     </main>
   );
 }
